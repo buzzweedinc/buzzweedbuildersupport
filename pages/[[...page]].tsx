9a1406a50a955a9b41b13d617b0a08ca087b5b7d
Ryan Ross <ryanross@Ryans-MacBook-Pro.local>
2023-06-01 15:17:20 +0200

    “searchadded17”

--- pages/[[...page]].tsx
+++ pages/[[...page]].tsx
@@ -1,38 +1,56 @@
 import React from 'react';
 import { useRouter } from 'next/router';
-import { BuilderComponent, builder, useIsPreviewing } from '@builder.io/react';
+import { BuilderComponent, builder, Builder, useIsPreviewing } from '@builder.io/react';
 import DefaultErrorPage from 'next/error';
 import Head from 'next/head';
-
-import { Builder } from '@builder.io/react'
 import dynamic from 'next/dynamic';
+import type {
+  GetStaticPathsContext,
+  GetStaticPropsContext,
+  InferGetStaticPropsType,
+} from 'next';
+import { NextSeo } from 'next-seo';
+import builderConfig from '@config/builder';
+import { resolveBuilderContent } from '@lib/resolve-builder-content';
+import '../blocks/ProductGrid/ProductGrid.builder';
+import '../blocks/CollectionView/CollectionView.builder';
+import { useThemeUI } from '@theme-ui/core';
+import { getLayoutProps } from '@lib/get-layout-props';
+import { useAddItemToCart } from '@lib/shopify/storefront-data-hooks';
+import { useUI } from '@components/common/context';
+import Link from '@components/common/Link';
 
-// Replace with your Public API Key
-builder.init("2076971b9cab4505854debbf40944000");
+const isProduction = process.env.NODE_ENV === 'production';
 
-export async function getStaticProps({ params }: any) {
-  // Fetch the builder content
-  const page = await builder
-    .get('page', {
-      userAttributes: {
-        urlPath: '/' + (params?.page?.join('/') || ''),
-      },
-    })
-    .toPromise();
+builder.init(builderConfig.apiKey);
+
+Builder.registerComponent(
+  dynamic(() => import("../components/common/Searchbardemo")),
+  {
+    name: 'Search Bar',
+    inputs: [],
+    image: 'IconSearch'
+  }
+)
+
+export async function getStaticProps({ params, locale }: GetStaticPropsContext<{ path: string[] }>) {
+  const page = await resolveBuilderContent('page', locale, {
+    urlPath: '/' + (params?.path?.join('/') || ''),
+  })
 
   return {
     props: {
-      page: page || null,
+      page,
+      locale,
+      ...(await getLayoutProps()),
     },
-    revalidate: 5
-  };
+    revalidate: 5,
+  }
 }
 
-export async function getStaticPaths() {
-  // Get a list of all pages in builder
+export async function getStaticPaths({ locales }: GetStaticPathsContext) {
   const pages = await builder.getAll('page', {
-    // We only need the URL field
-    fields: 'data.url', 
+    fields: 'data.url',
     options: { noTargeting: true },
   });
 
@@ -42,36 +60,80 @@ export async function getStaticPaths() {
   };
 }
 
-Builder.registerComponent(
-    dynamic(() => import("../components/common/Searchbardemo")),
-    {
-      name: 'Search Bar',
-      inputs: [],
-      image: 'IconSearch'
-    }
-  )
-
-
-
-export default function Page({ page }: any): JSX.Element {
+export default function Page({ page, locale }: InferGetStaticPropsType<typeof getStaticProps>): JSX.Element {
   const router = useRouter();
+  const { theme } = useThemeUI();
+  const addToCart = useAddItemToCart();
   const isPreviewing = useIsPreviewing();
+  const { openSidebar } = useUI();
 
   if (router.isFallback) {
     return <h1>Loading...</h1>
   }
 
   if (!page && !isPreviewing) {
-    return <DefaultErrorPage statusCode={404} />
+    return (
+      <>
+        <Head>
+          <meta name="robots" content="noindex" />
+          <meta name="title"></meta>
+        </Head>
+        {Builder.isBrowser && <DefaultErrorPage statusCode={404} />}
+      </>
+    )
   }
 
+  const { title, description, image } = page?.data! || {};
   return (
-    <>
+    <div>
+      {title && (
+        <NextSeo
+          title={title}
+          description={description}
+          openGraph={{
+            type: 'website',
+            title,
+            description,
+            locale,
+            ...(image && {
+              images: [
+                {
+                  url: image,
+                  width: 800,
+                  height: 600,
+                  alt: title,
+                },
+              ],
+            }),
+          }}
+        />
+      )}
       <Head>
         <title>{page?.data.title}</title>
       </Head>
-      {/* Render the Builder page */}
-      <BuilderComponent model="page" content={page} />
-    </>
+      <BuilderComponent
+        options={{ includeRefs: true }}
+        model="page"
+        data={{ theme }}
+        context={{
+          productBoxService: {
+            addToCart,
+            navigateToCart() {
+              openSidebar()
+            },
+            navigateToProductPage(product: { handle: string }) {
+              router.push(`/product/${product.handle}`)
+            },
+          },
+        }}
+        renderLink={(props: any) => {
+          if (props.target === '_blank' || props.href?.startsWith('#')) {
+            return <Link as="a" {...props} />
+          }
+          return <Link {...props} as={Link} />
+        }}
+        content={page}
+      />
+    </div>
   );
 }
