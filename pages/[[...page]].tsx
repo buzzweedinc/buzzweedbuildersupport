59d0b7458e44e0c6136fc60e38c15d6c52a495f3
Ryan Ross <ryanross@Ryans-MacBook-Pro.local>
2023-06-01 14:54:57 +0200

    “searchadded16”

--- pages/[[...page]].tsx
+++ pages/[[...page]].tsx
@@ -43,7 +43,7 @@ export async function getStaticPaths() {
 }
 
 Builder.registerComponent(
-    dynamic(() => import('/Users/ryanross/Documents/nextjs-shopify-master/components/common/Searchbardemo')),
+    dynamic(() => import("/Users/ryanross/Documents/nextjs-shopify-master/components/common/Searchbardemo")),
     {
       name: 'Search Bar',
       inputs: [],
