64cff2641b3aea0cd63724636cea0bc71ed54eed
Ryan Ross <ryanross@Ryans-MacBook-Pro.local>
2023-06-01 15:47:32 +0200

    “searchadded19”

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,7 +10,7 @@ import '../blocks/ProductGrid/ProductGrid.builder'
 import '../blocks/CollectionView/CollectionView.builder'
 import '../blocks/ProductView/ProductView.builder'
 import '../blocks/CloudinaryImage/CloudinaryImage.builder'
-import '../components/common/searchbardemo.builder.js';
+import '../components/common/searchbardemo.builder';
 
 
 Builder.register('insertMenu', {
