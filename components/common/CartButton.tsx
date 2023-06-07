2a32ab41491e8bc174431fb8174a470b4876ca73
Ryan Ross <ryanross@Ryans-MacBook-Pro.local>
2023-06-07 13:43:28 +0200

    “customcartbutton4”

--- components/common/CartButton.tsx
+++ components/common/CartButton.tsx
@@ -4,14 +4,20 @@ import { Bag } from '@components/icons'
 import { useUI } from '@components/common/context'
 
 interface CartButtonProps {
-  ariaLabel?: string
+  ariaLabel?: string,
+  buttonStyle?: React.CSSProperties, // Add this
 }
 
-const CartButton: React.FC<CartButtonProps> = ({ ariaLabel = 'Cart' }) => {
+const CartButton: React.FC<CartButtonProps> = ({ ariaLabel = 'Cart', buttonStyle }) => {
   const { openSidebar } = useUI()
   return (
     <div className="cart-button-container">
-      <button onClick={openSidebar} aria-label={ariaLabel} className="cart-button">
+      <button 
+        onClick={openSidebar} 
+        aria-label={ariaLabel} 
+        className="cart-button" 
+        style={buttonStyle} // Use it here
+      >
         <Bag />
       </button>
     </div>
@@ -20,5 +26,6 @@ const CartButton: React.FC<CartButtonProps> = ({ ariaLabel = 'Cart' }) => {
 
 export default CartButton
 export {}
+
 
 
