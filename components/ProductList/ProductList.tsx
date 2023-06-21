12d4e3bdfd3ef4a306de16e07c87a2e3260ee69a
Ryan Ross <ryan@buzzweed.com>
2023-06-21 13:49:01 +0200

    “ProductListAdded2”

--- components/ProductList/ProductList.tsx
+++ components/ProductList/ProductList.tsx
@@ -1,25 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import styles from './ProductList.module.css'; // Update this as necessary
 
-interface ProductType {
-  id: string;
-  type_name: string;
-}
-
 interface Product {
-  id: string;
+  id: number;
   product_name: string;
-  product_description: string;
-  product_url: string; // replace this with actual image URL property if it exists
+  handle: string;
   price: string;
-  type: ProductType;
-  // add any other properties returned in the product object that you need
+  vendor: string;
+  seller_name: string;
+  vendor_handle: string;
+  product_policy: null | string;
+  custom_fields: any[];
+  images: string;
+  review_avg: string;
 }
 
 const ProductList: React.FC = () => {
   const [products, setProducts] = useState<Product[]>([]);
 
-  // Load the products when the component mounts
   useEffect(() => {
     fetch('https://mvmapi.webkul.com/api/v2/public/products.json?limit=50&sort_by=date_add&sort_order=desc&shop_name=3e57b7.myshopify.com', {
       method: 'GET',
@@ -35,7 +33,6 @@ const ProductList: React.FC = () => {
   }, []);
 
   if (!products) {
-    // The data hasn't been loaded yet, so render a loading indicator or return null
     return <div>Loading products...</div>;
   }
 
@@ -43,10 +40,11 @@ const ProductList: React.FC = () => {
     <div className={styles.productList}>
       {products.map(product => (
         <div className={styles.productCard} key={product.id}>
-          <img src={product.product_url} alt={product.product_name} className={styles.productImage} /> {/* Replace with actual image property if it exists */}
+          <img src={product.images} alt={product.product_name} className={styles.productImage} />
           <h2 className={styles.productTitle}>{product.product_name}</h2>
-          <p className={styles.productDescription}>{product.product_description}</p>
+          <p className={styles.productVendor}>Vendor: {product.vendor}</p>
           <p className={styles.productPrice}>Price: {product.price}</p>
+          <p className={styles.productReview}>Average Review: {product.review_avg}</p>
           {/* Add more product details as needed */}
         </div>
       ))}
@@ -55,4 +53,5 @@ const ProductList: React.FC = () => {
 }
 
 export default ProductList;
+
 
